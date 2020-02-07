0d8c5122af95ec8438d06a43da10b81afac70927
“RajeeSri” <rajsri.j@gmail.com>
2020-02-07 15:36:23 -0500

    Initial Commit

---
+++ XMLParse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XMLParse
+//
+//  Created by YaathmiAR on 2/7/20.
+//  Copyright © 2020 YaathmiAR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XMLParse/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  XMLParse
+//
+//  Created by YaathmiAR on 2/7/20.
+//  Copyright © 2020 YaathmiAR. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
